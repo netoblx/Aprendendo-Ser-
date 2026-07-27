4ac27bceeaee71a39d189d2f567ba2b6f76e5e7e
netoblx <lucianoevangelistasantosneto@gmail.com>
2026-07-27 14:38:49 -0300

    Create: arquivocentral.cpp

---
+++ Capitulo 2/arquivo que usa código de outros arquivos/arquivocentral.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+#include "arquivoadicionalaoarquivocentral.h"
+#include "funcoesparaarquivocentral.cpp"
+
+int main() {
+    std::cout << "A soma e: " << somar(3, 4) << '\n';
+    std::cout << "A area e: " << calcular_area(5, 10) << '\n';
+    return 0;
+}
+
